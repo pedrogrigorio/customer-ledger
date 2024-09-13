302463723203278c692fb204ee2db6e16f0e09d6
Pedro Grigorio <pedroogrigorio@gmail.com>
2024-09-13 16:38:58 -0300

    config web environment

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -1,19 +1,38 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      borderColor: {
+        primary: '#D9D9D9',
+      },
+      textColor: {
+        primary: '#333437',
+        secondary: '#5C5E64',
+        terciary: '#767D92',
+        placeholder: '#ABB0B4',
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        button: {
+          primary: {
+            DEFAULT: '#7741FB',
+            hover: '#6b3be2',
+          },
+        },
+        table: {
+          header: '#F9F8FA',
+        },
+        sidebar: {
+          hover: '#F6F6F6',
+        },
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
